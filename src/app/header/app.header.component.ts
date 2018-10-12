04191cccce4c43b4f516a2c977889ee9ddf8122c
Iventyeva <julia.iventyeva@office.auriga.msk>
2018-10-12 17:44:16 +0300

    Missed file app.header.component.ts was added

--- src/app/header/app.header.component.ts
+++ src/app/header/app.header.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: '<div>sdgsag</div>'
+    templateUrl: '\app.header.component.html',
+    styleUrls: ['\app.header.component.scss']   
   })
   export class AppHeader {
   }
